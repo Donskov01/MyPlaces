03731357663be1c4e73f85d2f6b69a11e83560e3
Fedor Donskov <fedya_donskov@mail.ru>
2021-12-07 12:46:14 +0300

    Initial Commit

---
+++ MyPlaces/CustomTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTableViewCell.swift
+//  MyPlaces
+//
+//  Created by Fedor Donskov on 07.12.2021.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet var imageOfPlace: UIImageView!
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var locationLable: UILabel!
+    @IBOutlet var typeLabel: UILabel!
+    
+}
